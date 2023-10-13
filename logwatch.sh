b63acb223574caea4bc40169f8e8d99bc05727cd
Daniel Dowse <dev@daemonbytes.net>
2023-10-13 00:00:41 +0000

    rewritten shell code

--- logwatch.sh
+++ logwatch.sh
@@ -1,81 +1,90 @@
 #!/bin/sh
-set -x 
 
 CONFIG_DIR=$HOME/.config/proxy
 
 if [ ! -d $CONFIG_DIR ];then
-	mkdir -p $CONFIG_DIR
-	if [ ! -s $CONFIG_DIR/smtp.conf ]; then
-		echo "Missing SMTP configuration"
-		exit
-	fi
+mkdir -p $CONFIG_DIR
+if [ ! -s $CONFIG_DIR/smtp.conf ]; then
+echo "Missing SMTP configuration"
+exit
+fi
 else
 . $CONFIG_DIR/smtp.conf
 fi
 
-ACCESS_LOG="/var/log/squid/access.log"
+ACCESS_LOG=${1:-"/var/log/squid/access.log"}
 BLACKLIST="$CONFIG_DIR/blacklist"
 REPORT_MAIL="/tmp/report_mail"
 
 pull() {
-cat $1 | awk '/'"$2"'/{print $1" "$2" "$3" "$4" "$6 }'
+	cat $ACCESS_LOG | awk '/'"$1"'/{print $1" "$2" "$3" "$4" "$6 }' \
+	| uniq -f 4
+}
+
+hits() {
+	awk '/'"$1"'/{print $1" "$2" "$3" "$4" "$6 }' $ACCESS_LOG \
+	| uniq -f 4 | wc -l
 }
 
 report() {
-	echo "Sending..."
-	smtp-cli --missing-modules-ok -4 \
-	--server=$SMTP_HOST \
-	--port=$SMTP_PORT \
-	--hello-host=$SMTP_HELLO \
-	--user=$SMTP_USER \
-	--pass=$SMTP_AUTH \
-	--mail-from=$SMTP_FROM \
-	--rcpt-to=$SMTP_TO \
-	--charset=UTF-8 \
-	--subject="$1" \
-	--body-plain=$REPORT_MAIL
+		#smtp-cli --missing-modules-ok -4 \
+		smtp-cli --print-only --missing-modules-ok -4 \
+		--server=$SMTP_HOST \
+		--port=$SMTP_PORT \
+		--hello-host=$SMTP_HELLO \
+		--user=$SMTP_USER \
+		--pass=$SMTP_AUTH \
+		--mail-from=$SMTP_FROM \
+		--rcpt-to=$SMTP_TO \
+		--charset=UTF-8 \
+		--subject="$1" \
+		--body-plain=$REPORT_MAIL >/dev/null
 }
 
 if [ -s $BLACKLIST ]; then
-	i=`cat $BLACKLIST | sed '/^#/d' |  wc -l`
+s=`cat $BLACKLIST |  wc -l`
 else
-	echo "Blacklist empty"
-	exit
+echo "Blacklist empty"
+exit
 fi
 
 if [ -s $ACCESS_LOG ]; then
-	while [ "$i" -gt "0" ]; do
-			x=0
-			export KEYWORD=`sed -n "$i"p  $BLACKLIST`
-  			#pull $ACCESS_LOG $KEYWORD | uniq -f 4
-  			for z in `pull $ACCESS_LOG $KEYWORD | uniq -f 4`
-			do 
-				echo Match
-				if [ $x -ne "5" ]; then 
-				export z"$x"="$z"
-  				x=`expr $x + 1`
-				fi
-			
-				cat <<- EOF > $REPORT_MAIL
-				IP=$z0
-				HOSTNAME=$z1
-				MAC=$z2
-				DATE=$z3
-				STRING=$z4
-				KEYWORD=$KEYWORD
-				EOF
-				report "Match: HOST: $z1 (IP: $z0) Keyword: $KEYWORD"
-			done
-  	i=`expr $i - 1`
-	done
-fi 
+	while [ "$s" -gt "0" ]; 
+	do
+		export KEYWORD=`sed -n "$s"p  $BLACKLIST`
+		s=`expr $s - 1`
+		IFS=$'\n'
+		echo "$KEYWORD `hits $KEYWORD` hits"
+		for k in `pull $KEYWORD`
+			do
+			IFS=$' \t
+'
+			cat <<- EOF > $REPORT_MAIL
+			IP=`echo $k | cut -f 1 -d" "`
+			HOSTNAME=`echo $k | cut -f 2 -d" "`
+			MAC=`echo $k | cut -f 3 -d" "`
+			DATE=`echo $k | cut -f 4 -d" "`
+			URL=`echo $k | cut -f 5 -d" "`
+			EOF
+			IP=`echo $k | cut -f 1 -d" "`
+			HOSTNAME=`echo $k | cut -f 2 -d" "`
 
-if [ -s $REPORT_MAIL ]; then
-	rm $REPORT_MAIL
-	echo "Rotating logs"
-	/usr/local/sbin/squid -k rotate
+                       report "Match: $HOSTNAME (IP: $IP) Keyword: $KEYWORD"
+
+		done
+	done
 
 else
-	echo "No match found"
+	echo "No Accesslog found"
+	exit
+fi
+
+if [ -s $REPORT_MAIL ]; then
+rm $REPORT_MAIL
+echo "Rotating logs"
+/usr/local/sbin/squid -k rotate
+
+else
+echo "No match found"
 fi
 
