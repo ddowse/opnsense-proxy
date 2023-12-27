4813808b0325025de6063a5b3bb26f789349091f
Daniel Dowse <dev@daemonbytes.net>
2023-12-27 15:50:48 +0000

    Change backup file location

--- patch.sh
+++ patch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp /usr/local/opnsense/service/templates/OPNsense/Proxy/squid.conf /root/proxy-squid.conf.`date +%s`-BACKUP
+cp /usr/local/opnsense/service/templates/OPNsense/Proxy/squid.conf /var/backups/proxy-squid.conf.`date +%s`
 patch /usr/local/opnsense/service/templates/OPNsense/Proxy/squid.conf < squid.conf.patch
 configctl template reload OPNsense/Proxy
